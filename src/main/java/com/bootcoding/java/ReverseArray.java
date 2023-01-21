0930f3d6af4a2cd8aef053eb2f0a1ec0228ec942
NikhilDahale <nikhil.dahale97@gmail.com>
2023-01-21 11:28:48 +0530

    #Java-project-traning -java programs

---
+++ src/main/java/com/bootcoding/java/ReverseArray.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int n []={10,15,20,25,35,75,80,95};
+//        arrayreverse(n[]);
+
+    }
+
+
+    public static void arrayreverse(int n) {
+
+        int tem
+
+    }
+}
